b86d832faf60ac61a187696d83457697d19726b0
isamyakt <isamyakt17@gmail.com>
2022-11-01 22:34:07 +0530

    getting balance of specific token by using token address

--- getTokens.js
+++ getTokens.js
@@ -10,7 +10,7 @@ const TOKEN_PROGRAM = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA';
 const getTokens = async () => {
     try {
         const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
-        const tokenAccounts = await connection.getParsedTokenAccountsByOwner(new PublicKey("AVyZTBb5tWvSAAQMEqVr6WpgKc9rbkvQYuYfBL9YVUR1"),
+        const tokenAccounts = await connection.getParsedTokenAccountsByOwner(new PublicKey("DxKc73eJX5J1kY5ND69hnLs7ox64Q2exN3BVUWxBtBjo"),
             {
                 programId: new PublicKey(TOKEN_PROGRAM),
             }
@@ -21,9 +21,17 @@ const getTokens = async () => {
         );
 
         // let mapAccountData = nonZeroAccounts.map((obj) => obj.account.data);
-        let mapAccountData = nonZeroAccounts.map((obj) => obj.account.data.parsed.info);
+        // let mapAccountData = nonZeroAccounts.map((obj) => obj.account.data.parsed.info);
 
-        console.log(mapAccountData);
+        //Solana Tokens Mint public key https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json
+        
+        for (let acct of nonZeroAccounts) {
+            if (acct.account.data.parsed.info.mint == "8o66EVAf4u2Hr21m2tuRrPtEXFPLr8G8aL1ETStP8fDu") {
+                console.log(`VIBE token balance is ${acct.account.data.parsed.info.tokenAmount.amount / LAMPORTS_PER_SOL}`)
+            }
+        }
+
+        // console.log(mapAccountData);
 
         // console.log(nonZeroAccounts);
 
